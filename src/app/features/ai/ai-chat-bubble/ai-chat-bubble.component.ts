438ca3c6a974b4fb62e4d3f2964040269c726f6c
brahmi666 <forgamingkool@gmail.com>
2026-05-12 16:13:57 +0100

    cleanup: remove all spec files and add ai chat components

---
+++ src/app/features/ai/ai-chat-bubble/ai-chat-bubble.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-chat-bubble',
+  imports: [],
+  templateUrl: './ai-chat-bubble.component.html',
+  styleUrl: './ai-chat-bubble.component.css'
+})
+export class AiChatBubbleComponent {
+
+}
---
+++ src/app/features/ai/ai-chat-page/ai-chat-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-chat-page',
+  imports: [],
+  templateUrl: './ai-chat-page.component.html',
+  styleUrl: './ai-chat-page.component.css'
+})
+export class AiChatPageComponent {
+
+}
